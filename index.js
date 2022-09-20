4c34c11a6621d94f063a6d72ef34ce0683c82847
leonid riznyk <riznyk.leo@gmail.com>
2022-09-20 19:48:37 +0900

    WeakSet, WeakMap, Object methods

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Map and Set
-
+//MAP AND SET
 
 //filter unique values from array
 function unique(arr) {
@@ -19,4 +18,47 @@ function removeAnagram(arr){
     return Array.from(map.values());
 }
 let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
-console.log(removeAnagram(arr))
+//---------------------------------
+
+
+//WEAKMAP AND WEAKSET
+
+//How to track unread messages
+
+let messages = [
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"}
+];
+let readMessages = new WeakSet();
+// alert("Read message 0: " + readMessages.has(messages[0])); // true
+messages.shift();
+//---------------------------------
+
+
+//OBJECT METHODS
+
+//Sum all Salaries
+function sumSalaries(obj){
+    let sum = 0;
+    for (let salary of Object.values(obj)){
+        sum+= salary
+    }
+    return sum;
+}
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+};
+
+// find all object properties
+function propCount(obj){
+    return Object.keys(obj).length
+}
+let user = {
+    name: 'John',
+    age: 30
+};
+// alert( propCount(user) ); // 2
+
