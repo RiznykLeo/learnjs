4a6f41accbbdc7796fb272a594230de08e58096b
leonid riznyk <riznyk.leo@gmail.com>
2022-09-28 17:44:40 +0900

    async await

--- index.js
+++ index.js
@@ -410,3 +410,48 @@
 //
 // alert(err instanceof FormatError);
 // alert(err instanceof SyntaxError);
+
+// ASYNCHRONOUS JS
+
+//async/await
+// class HttpError extends Error {
+//     constructor(response) {
+//         super(`${response.status} for ${response.url}`);
+//         this.name = 'HttpError';
+//         this.response = response;
+//     }
+// }
+//
+// async function loadJson(url) {
+//     let response = await fetch(url);
+//     if (response.status == 200) {
+//         return response.json();
+//     } else {
+//         throw new HttpError(response);
+//     }
+// }
+//
+// async function demoGithubUser() {
+//
+//     let user;
+//     while (true) {
+//         let name = prompt("Login", "iliakan");
+//
+//         try {
+//             user = await loadJson(`https://api.github.com/users/${name}`);
+//             alert(`Полное имя: ${user.name}.`);
+//             break;
+//         } catch (err) {
+//             if (err instanceof HttpError && err.response.status === 404) {
+//                 alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+//             } else {
+//                 throw err;
+//             }
+//         }
+//     }
+//     return user;
+// }
+//
+// demoGithubUser();
+
+
